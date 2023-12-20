0d432fdcea99a2abfed50063cfe5f644f08b4300
Evgeniy Merzliakov <evgeniy_mq@mail.ru>
2023-12-20 17:29:27 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Hello world");
+
+
+int value = 100;
+Console.WriteLine(value);
+
+
+double a = 5.55;
+char b = '!';
+bool c = true;
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+
